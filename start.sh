219b804981f6891f2fee7576836322ffa2621b5e
movietimenow <160767940+movietimenow@users.noreply.github.com>
2024-02-22 08:01:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MTFileShareBot
 fi
-cd /Advance-File-Share-Bot
+cd /MTFileShareBot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
